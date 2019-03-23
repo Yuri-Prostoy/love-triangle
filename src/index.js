b37ac4b237a2c9c3953419a2871cae287a1f8932
Yuri <zhukovscy0@mail.ru>
2019-03-23 14:46:13 +0300

    compete love-triangle task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let trg = [...preferences],
+    triangleCount = 0;
+
+  if (trg.length >= 3) {
+    for (let i = 0; i < trg.length; i++) {
+      if (trg[i] !== "X") {
+        if ((trg[trg[trg[i] - 1] - 1] === i + 1) & (trg[i] !== i + 1)) {
+          triangleCount++;
+          trg[i] = trg[trg[i] - 1] = trg[trg[trg[i] - 1] - 1] = "X";
+        }
+      }
+    }
+  }
+  return triangleCount;
+
 };
